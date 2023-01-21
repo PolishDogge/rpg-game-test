473e8ea727c07ec6fc1391e2cd719f9b461b6ad4
PolishDogge <75747966+PolishDogge@users.noreply.github.com>
2023-01-21 17:50:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from inv_shop import *
 from item_descriptions import *
 from character import *
 
-            #name, health, move, armour, armourtype, dodge, critical, inventory, race, level, xp, gold, tag
+            #name, max health , health, move, armour, armourtype, dodge, critical, inventory, race, level, xp, gold, tag
 mc = character("Doggo", 100 ,100, (moves("Punch", 50, "Physical"), moves("Kick", 15, "Physical"), moves("Slash", 15, "Physical")), 5, 0, 1, 1,[], race("human"), 0, 0, 0,"Player")
 skeleton = character("Skeleton", 100, 100, moves("Slash", 10, "Physical"), 0, 0, 1, 1,[], race("undead"), 0, 500, 10, "Enemy")
 
